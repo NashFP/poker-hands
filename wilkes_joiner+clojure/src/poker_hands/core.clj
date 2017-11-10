75b48ff67a78494aaa1b9065a502031ded497187
wilkes <wilkesjoiner@gmail.com>
2017-11-10 17:13:16 -0600

    parse sample data

--- wilkes_joiner+clojure/src/poker_hands/core.clj
+++ wilkes_joiner+clojure/src/poker_hands/core.clj
@@ -1,6 +1,29 @@
-(ns poker-hands.core)
+(ns poker-hands.core
+  (:require [clojure.string :as string]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def s "TH 8H 5C QS TC 9H 4D JC KS JS")
+
+(defn parse-rank [rank]
+  (let [special {"T" "10"
+                 "J" "11"
+                 "Q" "12"
+                 "K" "13"
+                 "A" "14"}]
+    (edn/read-string (get special rank rank))))
+
+(defn parse-card [[rank suite]]
+  [(-> rank str parse-rank)
+   (-> suite str keyword)])
+
+(defn parse-hands [s]
+  (partition 5
+             (map parse-card (string/split s #"\s"))))
+
+(defn read-hands [file-path]
+  (with-open [data (io/reader file-path)]
+    (->> data
+         line-seq
+         (map parse-hands)
+         doall)))
